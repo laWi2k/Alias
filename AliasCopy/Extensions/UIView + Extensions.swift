56d5c61458b2a8815086dadcc12778dfb321cd41
Daniel <neskdan812@gmail.com>
2022-07-31 16:39:44 +0300

    Fixed nav bar + added shadows

---
+++ AliasCopy/Extensions/UIView + Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIView + Extensions.swift
+//  AliasCopy
+//
+//  Created by Daniel on 31.07.2022.
+//
+
+import Foundation
---
+++ AliasCopy/Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  AliasCopy
+//
+//  Created by Daniel on 31.07.2022.
+//
+
+import Foundation
