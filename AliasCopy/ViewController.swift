36976d59f8242cc8c3b353ad7d96800b040b05e7
Daniel <neskdan812@gmail.com>
2022-07-31 15:36:37 +0300

    Initial Commit

---
+++ AliasCopy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AliasCopy
+//
+//  Created by Daniel on 31.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
